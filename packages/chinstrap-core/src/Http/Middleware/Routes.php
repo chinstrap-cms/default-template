22097dc882db23cb1a0db65a37e28a6693c467ae
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-24 17:54:11 +0100

    Added routes middleware

---
+++ packages/chinstrap-core/src/Http/Middleware/Routes.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Http\Middleware;
+
+use League\Route\Router;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class Routes implements MiddlewareInterface
+{
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (getenv('APP_ENV') == 'development') {
+            $this->router->get('/__clockwork/{request:.+}', 'Chinstrap\Core\Http\Controllers\ClockworkController::process');
+        }
+        $this->router->get('/images/[{name}]', 'Chinstrap\Core\Http\Controllers\ImageController::get');
+        $this->router->get('/[{name:[a-zA-Z0-9\-\/]+}]', 'Chinstrap\Core\Http\Controllers\MainController::index')
+               ->middleware(new \Chinstrap\Core\Http\Middleware\HttpCache())
+               ->middleware(new \Chinstrap\Core\Http\Middleware\ETag());
+        $this->router->post('/[{name:[a-zA-Z0-9\-\/]+}]', 'Chinstrap\Core\Http\Controllers\MainController::submit');
+        try {
+            $response = $this->router->dispatch($request);
+        } catch (\League\Route\Http\Exception\NotFoundException $e) {
+            $view = $this->container->get('Chinstrap\Core\Contracts\Views\Renderer');
+            $response = $view->render(
+                $this->container->get('response')->withStatus(404),
+                '404.html'
+            );
+        }
+        if (getenv('APP_ENV') == 'development') {
+            $clockwork = $this->container->get('Clockwork\Support\Vanilla\Clockwork');
+            $clockwork->requestProcessed();
+        }
+        return $response;
+    }
+}
