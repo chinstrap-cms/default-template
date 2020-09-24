4baf4b9d75de5832befc0cc307596c4a948d56f6
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-24 19:57:01 +0100

    Added middleware for HTTP Proxy cache

---
+++ packages/chinstrap-core/src/Http/Middleware/HttpProxyMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chinstrap\Core\Http\Middleware;
+
+use Http\Message\StreamFactory\DiactorosStreamFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use PublishingKit\HttpProxy\Client;
+
+final class HttpProxyMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $cache;
+
+    public function __construct(CacheItemPoolInterface $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $client = new Client(function ($request) {
+            return $handler->handle($request);
+        });
+        $proxy = new Proxy($client, $this->cache, new DiactorosStreamFactory());
+        return $proxy->handle($request);
+    }
+}
