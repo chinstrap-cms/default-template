72c7cac260ded1e0912574dfdcb93014f12ca0b8
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-09-25 12:18:59 +0100

    Moved most remaining content to app factory class

--- public/index.php
+++ public/index.php
@@ -3,18 +3,11 @@
 declare(strict_types=1);
 
 use Chinstrap\Core\Exceptions\ErrorHandler;
-use Chinstrap\Core\Http\Middleware\ClockworkMiddleware;
-use Chinstrap\Core\Http\Middleware\HttpCachingProxyMiddleware;
-use Chinstrap\Core\Http\Middleware\NotFoundMiddleware;
-use Chinstrap\Core\Http\Middleware\RoutesMiddleware;
-use Chinstrap\Core\Http\Middleware\WhoopsMiddleware;
-use Chinstrap\Core\Kernel\Kernel;
+use Chinstrap\Core\Kernel\AppFactory;
+use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Laminas\HttpHandlerRunner\RequestHandlerRunner;
-use Laminas\Stratigility\MiddlewarePipe;
-use League\Container\Container;
-use League\Container\ReflectionContainer;
 
 require_once __DIR__ . '/../bootstrap.php';
 
@@ -22,17 +15,8 @@ if (!defined('PUBLIC_DIR')) {
     define('PUBLIC_DIR', __DIR__);
 }
 
-$app = new MiddlewarePipe();
-
-$kernel = new Kernel();
-$kernel->bootstrap();
-$container = $kernel->getContainer();
-
-$app->pipe($container->get(WhoopsMiddleware::class));
-$app->pipe($container->get(ClockworkMiddleware::class));
-$app->pipe($container->get(HttpCachingProxyMiddleware::class));
-$app->pipe($container->get(RoutesMiddleware::class));
-$app->pipe($container->get(NotFoundMiddleware::class));
+$factory = new AppFactory();
+$app = $factory();
 
 $server = new RequestHandlerRunner(
     $app,
@@ -40,7 +24,7 @@ $server = new RequestHandlerRunner(
     static function () {
         return ServerRequestFactory::fromGlobals();
     },
-    $container->get(ErrorHandler::class)
+    new ErrorHandler(new ResponseFactory())
 );
 
 $server->run();
