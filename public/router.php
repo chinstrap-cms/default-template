7edef98a597e80f6e283ec4913d7121033260511
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-12-19 22:25:54 +0000

    Fixed a bug that stopped fonts being served

--- public/router.php
+++ public/router.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 if (php_sapi_name() == "cli-server") {
+    if (preg_match('/\.(?:woff2?)(\?[a-zA-Z0-9]+)?$/', $_SERVER["REQUEST_URI"])) {
+        return false;
+    }
     if (preg_match('/\.(?:css|js|json)(\?id=[a-zA-Z0-9]+)?$/', $_SERVER["REQUEST_URI"])) {
         return false;
     }
