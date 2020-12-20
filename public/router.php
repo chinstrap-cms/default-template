7c4e287ab46178af59b370076e56b9589da78ca7
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-12-20 19:03:54 +0000

    Amended router to allow images

--- public/router.php
+++ public/router.php
@@ -6,7 +6,7 @@ if (php_sapi_name() == "cli-server") {
     if (preg_match('/\.(?:woff2?)(\?[a-zA-Z0-9]+)?$/', $_SERVER["REQUEST_URI"])) {
         return false;
     }
-    if (preg_match('/\.(?:css|js|json)(\?id=[a-zA-Z0-9]+)?$/', $_SERVER["REQUEST_URI"])) {
+    if (preg_match('/\.(?:css|js|json|tiff|gif|png|jpe?g)(\?id=[a-zA-Z0-9]+)?$/', $_SERVER["REQUEST_URI"])) {
         return false;
     }
 }
