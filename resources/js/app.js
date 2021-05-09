e364a609f8d2376b2088ecc7b14d9d965d57bd5f
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-05-09 12:38:20 +0100

    Some progress integrating Alpine.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,39 @@
 import Fuse from 'fuse.js';
+import 'alpinejs';
 
-try {
-  window.$ = window.jQuery = require('jquery');
+function navHeader() {
+  return {
+    initSearch() {
+      fetch(`/storage/index.json`)
+        .then((response) => {
+          return response.json();
+        }).then((json) => {
+          const fuse = new Fuse(json, {
+            keys: ['title'],
+            shouldSort: true
+          });
+        });
+    },
+    updateSearch(event) {
+      debugger;
+    }
+  };
+};
+// $('#search').on('keyup', function () {
+//   let result = fuse.search($(this).val());
 
-  $(document).ready(function () {
-    window.$.getJSON('/storage/index.json', function (response) {
-      const fuse = new Fuse(response, {
-        keys: ['title'],
-        shouldSort: true
-      });
-      $('#search').on('keyup', function () {
-        let result = fuse.search($(this).val());
-
-        // Output it
-        let resultdiv = $('ul.searchresults');
-        if (result.length === 0) {
-          // Hide results
-          resultdiv.hide();
-        } else {
-          // Show results
-          resultdiv.empty();
-          for (let item in result.slice(0,4)) {
-            let searchitem = '<li><a href="' + result[item].url + '">' + result[item].title + '</a></li>';
-            resultdiv.append(searchitem);
-          }
-          resultdiv.show();
-        }
-
-      });
-    });
-  });
-} catch (e) {
-  console.log(e);
-}
+//   // Output it
+//   let resultdiv = $('ul.searchresults');
+//   if (result.length === 0) {
+//     // Hide results
+//     resultdiv.hide();
+//   } else {
+//     // Show results
+//     resultdiv.empty();
+//     for (let item in result.slice(0,4)) {
+//       let searchitem = '<li><a href="' + result[item].url + '">' + result[item].title + '</a></li>';
+//       resultdiv.append(searchitem);
+//     }
+//     resultdiv.show();
+//   }
+// });
