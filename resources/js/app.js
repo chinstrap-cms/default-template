57b534e75d5e0a88f72c47b8d915d5b27a4c893a
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-05-09 13:39:56 +0100

    Tidied up JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,21 +25,3 @@ window.navHeader = function () {
     items: [],
   };
 };
-// $('#search').on('keyup', function () {
-//   let result = fuse.search($(this).val());
-
-//   // Output it
-//   let resultdiv = $('ul.searchresults');
-//   if (result.length === 0) {
-//     // Hide results
-//     resultdiv.hide();
-//   } else {
-//     // Show results
-//     resultdiv.empty();
-//     for (let item in result.slice(0,4)) {
-//       let searchitem = '<li><a href="' + result[item].url + '">' + result[item].title + '</a></li>';
-//       resultdiv.append(searchitem);
-//     }
-//     resultdiv.show();
-//   }
-// });
