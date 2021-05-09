52fe837abb46030963d9340a50a8ec502afacd29
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2021-05-09 11:41:53 +0100

    Fixed config and amended Psalm setup

--- config/local.php
+++ config/local.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
 return [
-    'base_url' => getenv('BASE_URL'),
+    'base_url' => $_ENV['BASE_URL'],
+    'key' => $_ENV['APP_KEY'],
+    'session_time' => $_ENV['SESSION_TIME'],
 ];
